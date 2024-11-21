d9641d62abde4b79b876d8e346e94180712f82c3
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-11-21 11:13:08 +0100

    Publish

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 import Generate from "./index.js";
+const generated = await Generate("https://www.youtube.com/watch?v=3G0a7r4b7Eg", "test")
 
-console.log(await Generate("https://www.youtube.com/watch?v=3G0a7r4b7Eg", "test"))
+if (generated != "https://ably-yt.vercel.app/?channel=test_3G0a7r4b7Eg&videoId=3G0a7r4b7Eg") {
+    throw new Error("Failed to check, given https://ably-yt.vercel.app/?channel=test_3G0a7r4b7Eg&videoId=3G0a7r4b7Eg, recieved " + generated)
+}
